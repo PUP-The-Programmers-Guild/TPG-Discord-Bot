11dea19aaef6e54f6ef213556258da5b9e626f6e
Nicholas Noly C. dela Torre <88265052+Nicktacku@users.noreply.github.com>
2023-08-25 12:05:02 +0800

    soft implementation of sql database

--- cogs/Logs.py
+++ cogs/Logs.py
@@ -1,8 +1,11 @@
 from discord.ext import commands
 import csv
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
+import csv
+import sqlite3
 
+from discord.utils import time_snowflake
 
 class Logs(commands.Cog):
     def __init__(self, client):
@@ -40,7 +43,7 @@ class Logs(commands.Cog):
                 print(updated_row)
 
             if worked:
-                await message.add_reaction("💚")
+                # await message.add_reaction("💚")
                 print("added emoji")
             if is_time:
                 with open(PATH, "w", newline="") as file:
@@ -56,7 +59,7 @@ class Logs(commands.Cog):
                         "Date": str(time),
                         "Days": 1
                     })
-                await message.add_reaction("💚")
+                # await message.add_reaction("💚")
                 print("new added emoji")
 
         channels = ['1045330081175842887', '1139959499193598002']
@@ -67,29 +70,102 @@ class Logs(commands.Cog):
         if right_channel and (right_prefix or bolded_prefix):
             author = ctx.author
             user_id = author.id
-            now = datetime.now().strftime("%m/%d/%Y")
+            time = datetime.now().strftime("%m/%d/%Y")
             # message_id = ctx.id
-            await log(str(author), str(user_id), str(now), ctx)
+            await log(str(author), str(user_id), str(time), ctx)
+
+            # sql implementation
+            conn = sqlite3.connect("database/100DOC.db")
+            c = conn.cursor()
+
+            c.execute(f"""
+              SELECT Discord_Name, Date, Streak_Count
+              FROM Logs l INNER JOIN Users u
+                ON l.Discord_ID = u.Discord_ID
+              WHERE Discord_Name = '{author}'
+              """)
+            user = c.fetchall()
+            streak = 0
+            print(user)
+
+            yesterday = (datetime.now() - timedelta(1)).strftime("%m/%d/%Y")
+            if user[0][1] == yesterday:
+                streak = user[0][2] + 1
+            if user and user[0][1] != time:
+                c.execute(f"""
+                        UPDATE Logs
+                        SET Log_Count = Log_Count + 1,
+                        Date = '{time}',
+                        Streak_Count = {streak}
+                        WHERE Discord_ID = {user_id}""")
+                conn.commit()
+                await ctx.add_reaction("💚")
+            elif not user:
+                c.execute(f"""
+                INSERT INTO Logs (Discord_ID, Log_Count, Date, Streak, Streak_Count)
+                VALUES (
+                    {user_id},
+                    0,
+                    '{time}',
+                    0,
+                    1)""")
+                conn.commit()
+                c.execute(f"""
+                        INSERT INTO Users
+                        VALUES (
+                            {user_id},
+                            '{author}')""")
+                conn.commit()
+                await ctx.add_reaction("💚")
             print("it works")
 
     @commands.command()
     async def see_log(self, ctx):
-        in_records = False
-        days = 0
-        with open(r"cogs/database/log.csv", "r") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                print(row["Username"])
-                if str(ctx.author.id) == row["Discord ID"]:
-                    in_records = True
-                    days = row['Days']
-                    await ctx.channel.send(
-                        f"{ctx.author.display_name} logged in: {days} days out of 100")
-                    break
-        if not in_records:
-            await ctx.channel.send(
-                    f"{ctx.author.display_name} is still not in the records")
+        # in_records = False
+        # days = 0
+        # with open(r"cogs/database/log.csv", "r") as file:
+        #     reader = csv.DictReader(file)
+        #     for row in reader:
+        #         print(row["Username"])
+        #         if str(ctx.author.id) == row["Discord ID"]:
+        #             in_records = True
+        #             days = row['Days']
+        #             await ctx.channel.send(
+        #                 f"{ctx.author.display_name} logged in: {days} days out of 100")
+        #             break
+        # if not in_records:
+        #     await ctx.channel.send(f"{ctx.author.display_name} is still not in the records")
 
+        # sql implementation
+        channel = self.client.get_channel(1139959499193598002)
+        conn = sqlite3.connect("database/100DOC.db")
+        c = conn.cursor()
+        c.execute(f"""
+                  SELECT Log_Count
+                  FROM Logs
+                  WHERE Discord_ID = {ctx.author.id}""")
+        log_amount = c.fetchone()
+
+        if log_amount is not None:
+            await ctx.channel.send(f"{ctx.author.display_name} logged in: {log_amount[0]} days out of 100")
+        else:
+            await ctx.channel.send(f"{ctx.author.display_name} is still not in the records")
+
+    @commands.command()
+    async def db_see_log(self, ctx):
+      channel = self.client.get_channel(1045330081175842887)
+      conn = sqlite3.connect("database/100DOC.db")
+      c = conn.cursor()
+      c.execute(f"""
+                SELECT Log_Count
+                FROM Logs
+                WHERE Discord_ID = {ctx.author.id}""")
+      log_amount = c.fetchone()
+
+      if log_amount is not None:
+        await channel.send(f"{ctx.author.display_name} logged in: {days} days out of 100")
+      elif log_amount is None:
+        await channel.send(f"{ctx.author.display_name} is still not in the records")
 
 async def setup(client):
     await client.add_cog(Logs(client))
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
 import os
+import sqlite3
+import csv
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 from keep_alive import keep_alive
 
 # sets up intent that will be requested
@@ -11,12 +15,95 @@ intents.reactions = True
 # initializes the bot
 client = commands.Bot(command_prefix="+", intents=intents)
 
+def scheduled_message(msg):
+  print("updated:", datetime.now().strftime("%H:%M:%S"))
 
 @client.event
 async def on_ready():
     await load()
     await client.change_presence(activity=discord.CustomActivity(name='Welcome to the Guild'))
     print(f"logged on as {client.user}")
+    channel = client.get_channel(1143347614532767835)
+    scheduler = BackgroundScheduler()
+    job_id = scheduler.add_job(scheduled_message, "interval", hours=8, args= ["Hello"])
+    scheduler.start()
+    await channel.send("Im up")
+
+@client.command()
+async def compute_streak(ctx):
+    time_record = {}
+    point_record = {}
+    channel = client.get_channel(1045330081175842887)
+    print(channel)
+
+    async for text in channel.history(limit=1000):
+        time_stamp = text.created_at.strftime("%m/%d/%Y")
+        if time_stamp > "08/13/2023" and time_stamp[6:] != "2022":
+            user_name = text.author.id
+            if user_name in time_record:
+                print(f'time stamp: {time_stamp[3:5]} == {int(time_record[user_name][3:5]) - 1}')
+                if int(time_stamp[3:5]) == int(time_record[user_name][3:5]) - 1:
+                    point_record[user_name] += 1
+                    time_record[user_name] = time_stamp
+                    print("point_added")
+                elif int(time_stamp[3:5]) != int(time_record[user_name][3:5]) - 1 and not (int(time_stamp[3:5]) == int(time_record[user_name][3:5])):
+                    point_record[user_name] = 0
+                    time_record[user_name] = time_stamp
+                    print("point_resetted")
+            elif user_name not in time_record:
+                point_record[user_name] = 1
+                time_record[user_name] = time_stamp
+                print("not in record")
+            print(f"{user_name} has {point_record[user_name]} during {time_record[user_name]}")
+            print("-" * 30)
+
+    conn = sqlite3.connect("database/100DOC.db")
+    c = conn.cursor()
+    for name, points in point_record.items():
+      print("*" * 40)
+      print(name, points)
+      c.execute(f"""
+                UPDATE Logs
+                SET Streak_Count = {points}
+                WHERE Discord_ID = {name}""")
+    conn.commit()
+
+@client.command()
+async def update_db(ctx):
+    print("hello")
+    conn = sqlite3.connect("database/100DOC.db")
+    c = conn.cursor()
+    with open(r"cogs/database/log.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            c.execute(f"""
+                      UPDATE Logs
+                      SET
+                          Log_Count = {row['Days']},
+                          Date = '{row['Date']}'
+                      WHERE Discord_ID = {row['Discord ID']}""")
+
+            c.execute(f"""
+                      UPDATE Users
+                      SET Discord_ID = {row["Discord ID"]},
+                        Discord_Name = '{row["Username"]}'
+                      WHERE Discord_ID = {row["Discord ID"]}""")
+    conn.commit()
+    conn.close()
+    print("Worked")
+    await ctx.channel.send("Done")
+
+@client.command()
+async def notify_me(ctx, sched):
+    conn = sqlite3.connect("database/100DOC.db")
+    c = conn.cursor()
+
+    c.execute(f"""
+              INSERT INTO Notifications (Discord_ID, Scheduled_Time)
+              VALUES ({ctx.author.id},
+                      '{sched}')""")
+    conn.commit()
+    await ctx.channel.send("Done")
 
 initial_extensions = [
     f"cogs.{filename[:-3]}"
